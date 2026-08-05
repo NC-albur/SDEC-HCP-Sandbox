14187a2c47d6f01d4ad9d27eebb1bbedc42fd62b
NC-albur <albur@netcompany.com>
2026-08-05 12:28:36 +0100

    SDEC-545 - Made base class abstract

--- src/test/java/com/example/springdummyapp/PostgresBaseTest.java
+++ src/test/java/com/example/springdummyapp/PostgresBaseTest.java
@@ -8,7 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
-public class PostgresBaseTest {
+public abstract class PostgresBaseTest {
 
     @SuppressWarnings("resource")
     static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
