21627c7f2607b1dc0daa7d7ef203b0869f2dfe90
NC-albur <albur@netcompany.com>
2026-08-07 13:25:20 +0100

    SDEC-530 - Added JaCoCo coverage reporting

--- src/main/java/com/example/springdummyapp/SpringDummyAppApplication.java
+++ src/main/java/com/example/springdummyapp/SpringDummyAppApplication.java
@@ -2,13 +2,12 @@ package com.example.springdummyapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
 public class SpringDummyAppApplication {
 
     static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(SpringDummyAppApplication.class, args);
+        SpringApplication.run(SpringDummyAppApplication.class, args);
     }
 }
