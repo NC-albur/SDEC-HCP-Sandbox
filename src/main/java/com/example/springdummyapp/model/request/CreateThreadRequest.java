5516ee0137d30e6d0fdaec0456a1bc090e549901
oscar toomey <oscartoomey10@gmail.com>
2026-08-20 17:15:07 +0100

    added POST request handling

--- src/main/java/com/example/springdummyapp/model/request/CreateThreadRequest.java
+++ src/main/java/com/example/springdummyapp/model/request/CreateThreadRequest.java
@@ -1,3 +1,10 @@
 package com.example.springdummyapp.model.request;
+import java.time.LocalDateTime;
 
-public record CreateThreadRequest(String threadName) {}
+public record CreateThreadRequest(String threadName,
+                                  String id,
+                                  String threadReference,
+                                  String status,
+                                  String associatedCaseReference) {
+
+}
